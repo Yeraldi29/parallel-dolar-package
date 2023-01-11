4de2a5d403fc58e250c235523190fd0a75563fe7
Yeraldi29 <yeraldimujicarosales@gmail.com>
2023-01-11 19:09:20 -0400

    created parallelDolar package

--- src/parallelDolar.php
+++ src/parallelDolar.php
@@ -3,11 +3,34 @@ namespace Yeraldi29\parallelDolar;
 require '../vendor/autoload.php';
 
 use GuzzleHttp\Client;
-// https://api.exchangedyn.com/markets/quotes/usdves/dolartoday
-// https://api.exchangedyn.com/markets/quotes/usdves/bcv
-$client = new Client(['base_uri' => 'https://api.exchangedyn.com/markets/quotes/usdves/']);
 
-$response = $client->request('GET','bcv');
-$json = $response->getBody()->getContents();
+class ParallelDolar{
+    public function __construct($option){
+        $this->client = new Client(['base_uri' => 'https://api.exchangedyn.com/markets/quotes/usdves/']);
+        $this->option = $option;
+    }
+    
+    public function getDataJSON(){
+       return $this->queryApi('json');
+    }
 
-echo $json;
+    public function getDataValues(){
+        return $this->queryApi('getValues');
+    }
+    
+    private function queryApi($option){
+        if($this->option === 'bcv' || $this->option === 'dolartoday'){
+            $response = $this->client->request('GET',$this->option);
+            $json = $response->getBody()->getContents();
+
+            if($option === 'json'){
+                return $json;
+            }
+            if($option === 'getValues'){
+                return json_decode($json);
+            }
+        }
+
+        return;
+    }
+}
